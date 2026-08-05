f3e5d90396c62a28a95a47d8253e14ce85be7255
Luke Cuzzetto <lacuzzetto@gmail.com>
2026-08-05 09:45:45 -0700

    feat: add initial user migration

---
+++ migrations/00001_create_users_table.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+-- +goose Down
+DROP TABLE IF EXISTS users;
